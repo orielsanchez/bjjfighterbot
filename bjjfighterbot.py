75c30fb008ea03a5dbd490b3c299db5ea2e28ee1
Oriel Sanchez <deathofacolour@icloud.com>
2019-12-25 00:50:06 -0800

    added updates for terminal

--- bjjfighterbot.py
+++ bjjfighterbot.py
@@ -13,7 +13,7 @@ CLIENT_SECRET = config.CLIENT_SECRET
 PASSWORD = config.PASSWORD
 USERNAME = config.USERNAME
 target_sub = 'iamjacksheart'
-target_word = '!bjjfighter'
+target_word = '!bjjfighterbot'
 
 # reddit api login
 reddit = praw.Reddit(client_id=CLIENT_ID,
@@ -21,6 +21,7 @@ reddit = praw.Reddit(client_id=CLIENT_ID,
                      password=PASSWORD,
                      username=USERNAME,
                      user_agent=f'{target_word}bot by (/u/iamjacksheart)')
+print('Logged in')
 
 # subreddits you want your bot to live in
 subreddit = reddit.subreddit(target_sub)
@@ -32,14 +33,21 @@ with open('./fighters.csv', 'r') as f:
     for row in reader:
         result[row[0]] = row[1:]
 
+print('Opened fighters.csv')
+
 start_time = int(time.time())
+print('Start time is: ' +
+      str(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))))
 
 for comment in reddit.subreddit(target_sub).stream.comments():
     if start_time < int(comment.created_utc):
         body = comment.body
         if target_word in body:
             query = body.replace(f'{target_word} ', '')
+            print('Found target word... ' +
+                  str(time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(time.time()))))
             if query in result:
+                print('Processing request...')
                 message = query + ': ' + \
                     str(result.get(query)[1]) + \
                     "\n\n" + str(result.get(query)[0])
